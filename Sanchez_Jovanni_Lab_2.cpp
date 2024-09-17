34cbb8d7251ea1ed6ba8abb6e3c42a8acd159f95
Jovanni <jovannis8@nycstudents.net>
2024-09-17 17:58:52 -0400

    better

--- Sanchez_Jovanni_Lab_2.cpp
+++ Sanchez_Jovanni_Lab_2.cpp
@@ -29,15 +29,23 @@ int main(){
 
     //This is the start of part 2
     int desiredCalories; // This will be used to store the desired calories
-    const double CALORIE2OUNCE_CONVERSION_RATE = 0.004571; // This wont change, so its a const. 
-    // There is 0.004571 ounces/calorie. 
 
     // Get user input to how many calories are desired in order to 
-    // Divide it by the convertion rate in order to get ounces
+    // Divide it by cereal calories to get a serving and times it by the ounces
     cout << "How many calories of " << cerealName << " are you willing to eat today?: ";
     cin >> desiredCalories;
     cout << "This is how many ounces you should eat if you want " << desiredCalories;
-    cout << " calories: " << desiredCalories * CALORIE2OUNCE_CONVERSION_RATE << " ounces";                                                    
+    cout << " calories: " << (cerealCalories / cerealOunces) * desiredCalories << " ounces";                                                    
 
     return 0;
 }
+
+/*
+What is the name of your cereal?: oatmeal
+How many ounces of cereal do you have?: 50
+How many calories are in one serving?: 10
+
+One serving of oatmeal is 50 ounces and has 10 calories per ounces.
+How many calories of oatmeal are you willing to eat today?: 500    
+This is how many ounces you should eat if you want 500 calories: 2.2855 ounces
+*/
